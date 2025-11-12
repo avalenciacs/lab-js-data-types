03d3550bb2c3fd8364e1710b254d139d571e2508
Anderson Valencia <avalenciacs@gmail.com>
2025-11-12 15:47:01 +0100

    Solved lab

--- index.js
+++ index.js
@@ -12,8 +12,8 @@ const s5 = "and";
 
 // Print out the concatenated string
 
-
-
+const tongueTwister = s1 + ' ' + s2 + ' ' + s3 + ' ' + s4 + ' ' + s5;
+console.log(tongueTwister);
 
 /*******************************************
     Iteration 1.2 | Camel Tail
@@ -22,11 +22,9 @@ const part1 = "java";
 const part2 = "script";
 
 // Convert the last letter of part1 and part2 to uppercase and concatenate the strings
-
-
+const result = part1.slice(0, 1) + part1.slice(-1).toUpperCase() + part2.slice(0, -1) + part2.slice(-1).toUpperCase();
 // Print the cameLtaiL-formatted string
-
-
+console.log(result);
 
 
 /*******************************************
@@ -35,10 +33,12 @@ const part2 = "script";
 const billTotal = 84;
 
 // Calculate the tip (15% of the bill total)
-
+let propina = (billTotal / 100) * 15;
+let totalP = billTotal + propina;
 
 // Print out the tipAmount
 
+console.log(totalP);
 
 
 
@@ -47,11 +47,11 @@ const billTotal = 84;
 *******************************************/
 
 // Generate a random integer between 1 and 10 (inclusive)
-
+const randomNumber = Math.floor(Math.random()*10)+ 1;
 
 // Print the generated random number
 
-
+console.log(randomNumber);
 
 /*******************************************
     Iteration 3.1 | Booleans
@@ -62,15 +62,21 @@ const b = false;
 
 // Try and guess the output of the below expressions first and write your answers down:
 const expression1 = a && b;
-
+console.log(expression1);
 const expression2 = a || b;
+console.log(expression2);
 
 const expression3 = !a && b;
+console.log(expression3);
 
 const expression4 = !(a && b);
+console.log(expression4);
 
 const expression5 = !a || !b;
+console.log(expression5);
 
 const expression6 = !(a || b);
+console.log(expression6);
 
 const expression7 = a && a;
+console.log(expression7);
